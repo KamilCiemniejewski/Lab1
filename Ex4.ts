64d65525b2dc3f20e5d6ea0d068107890ebbbc65
KamilCiemniejewski <163150440+KamilCiemniejewski@users.noreply.github.com>
2025-02-04 09:12:27 +0000

    Ex4.ts

--- Ex4.ts
+++ Ex4.ts
@@ -1,3 +1,4 @@
+//Counting the total number of characters including spaces
 function countString(value: string)
 {
     return value.length;
@@ -5,27 +6,29 @@ function countString(value: string)
 
 console.log(countString(" test 1 "));
 
+//Couting the total number of characters excluding spaces
 function removeSpaces(value:string):number{
-    return value.trim().length;
+    return value.trim().length; //.trim() to remove counting spaces
 }
 
 console.log(removeSpaces(" test 1 "));
 
+//Counting the total number of characters including or excluding spaces
 function countBoth(value: string, spaces?:boolean):number
 {
     let num:number;
     if (spaces)
     {
-        num = value.length;
+        num = value.length; //Include spaces
     }
     else
     {
-        num = value.trim().length;
+        num = value.trim().length; //Exclude spaces
     }
 
     return num;
 }
 
-console.log(countBoth(" test 1 ", true));
-console.log(countBoth(" test 1 ", false));
-console.log(countBoth(" test 1 "));
+console.log(countBoth(" test 1 ", true)); //If is true it includes spaces
+console.log(countBoth(" test 1 ", false)); //If is false it excludes spaces
+console.log(countBoth(" test 1 ")); // //If underfined default set to false
